cfa707be8243c11182038544aac3c244552f3a27
Berk Aksu <berkaksu79@gmail.com>
2023-12-16 01:55:42 +0300

    Initial Commit

---
+++ LoadingAnimation1/LoadingAnimation1App.swift
@@ -0,0 +1,17 @@
+//
+//  LoadingAnimation1App.swift
+//  LoadingAnimation1
+//
+//  Created by Berk Aksu on 16.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct LoadingAnimation1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
