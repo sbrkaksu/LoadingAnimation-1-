c3cfaf2eccef6c2078710c10dd065bcc9bc8f2d6
Berk Aksu <berkaksu79@gmail.com>
2023-12-18 00:03:03 +0300

    States and the circles were defined

--- LoadingAnimation1/ContentView.swift
+++ LoadingAnimation1/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isCircleBig = [false, false, false]
+    @State private var delay = [0.0, 0.2, 0.4]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack{
+            ForEach(0..<3){index in
+                Circle()
+                    .foregroundColor(.gray)
+                    .frame(width: isCircleBig[index] ? 15 : 0, height: isCircleBig[index] ? 15 : 0)
+                    .animation(.easeIn(duration: 0.7).repeatForever(), value: isCircleBig[index])
+                    .onAppear{
+                        DispatchQueue.main.asyncAfter(deadline: .now() + delay[index]) {
+                            self.isCircleBig[index] = true
+                        }
+                    }
+            }
         }
-        .padding()
     }
 }
 
